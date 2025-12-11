8f2c1d16743ba12aa97c219df24908a0a07f53b1
colmeye <colmeyer1@gmail.com>
2025-12-11 14:41:44 -0600

    drawio update

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -6,18 +6,14 @@ const io = new Server();
 const ioBunEngine = new Engine({ path: "/socket.io/" });
 io.bind(ioBunEngine);
 
-io.on("connection", (socket) => {
-  console.log("connection", socket.id);
-});
-
 const app = new Elysia()
   .all("/socket.io/*", ({ request, server }) => {
     if (!server) throw new Error("Server not found");
     return ioBunEngine.handleRequest(request, server);
   })
-  .get("/", () => "Hello Elysia")
   .listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+app.onStart(({ server }) => {
+  if (!server) throw new Error("Server not found");
+  console.log(`🦊 Elysia is running at ${server.hostname}:${server.port}`);
+});
