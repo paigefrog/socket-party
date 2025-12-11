c2a8d1049fed08b2a7399edb6e54339cd635f00a
colmeye <colmeyer1@gmail.com>
2025-12-11 13:13:55 -0600

    elysia and socketio integration

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,16 +3,20 @@ import { Elysia } from "elysia";
 import { Server } from "socket.io";
 
 const io = new Server();
-const engine = new Engine({
-  path: "/socket.io/",
-});
+const engine = new Engine({ path: "/socket.io/" });
 io.bind(engine);
 
 io.on("connection", (socket) => {
   console.log("connection", socket.id);
 });
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .all("/socket.io/*", ({ request, server }) => {
+    if (!server) throw new Error("Server not found");
+    return engine.handleRequest(request, server);
+  })
+  .get("/", () => "Hello Elysia")
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
