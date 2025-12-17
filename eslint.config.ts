6acce7da0dd9411fdf9854ae545bd6bed25ea5b0
colmeye <colmeyer1@gmail.com>
2025-12-17 13:52:09 -0600

    eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,5 @@
 import js from "@eslint/js";
 import { defineConfig, globalIgnores } from "eslint/config";
-import importPlugin from "eslint-plugin-import";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
@@ -13,24 +12,4 @@ export default defineConfig([
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
-  {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      importPlugin.flatConfigs.recommended,
-      importPlugin.flatConfigs.typescript,
-    ],
-    rules: {
-      "import/order": [
-        "error",
-        {
-          alphabetize: {
-            caseInsensitive: false,
-            order: "asc",
-          },
-          groups: ["builtin", ["sibling", "parent"], "index", "object"],
-          "newlines-between": "always",
-        },
-      ],
-    },
-  },
 ]);
