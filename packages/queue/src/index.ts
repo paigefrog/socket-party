a5639292a96f8a6c5aba536f2b0857d8b4cbc99c
colmeye <colmeyer1@gmail.com>
2025-12-26 14:59:03 -0600

    queue lint

--- packages/queue/src/index.ts
+++ packages/queue/src/index.ts
@@ -1,9 +1,9 @@
 import type { SQSHandler } from "aws-lambda";
 
-export const handler: SQSHandler = async (event, context, callback) => {
+export const handler: SQSHandler = async (event, context) => {
   try {
     console.log("Received event:", JSON.stringify(event, null, 2));
   } catch (error) {
-    console.error("Error processing event:", error);
+    console.error("Error processing event:", error, context);
   }
 };
